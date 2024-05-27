da69a12b87ee0ef1b2055e270d3c73823067caf2
agipensador <agipensador@gmail.com>
2024-05-27 17:58:21 -0300

    gradle incluido modulo

--- WebViewAndroid/settings.gradle.kts
+++ WebViewAndroid/settings.gradle.kts
@@ -13,11 +13,21 @@ pluginManagement {
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    val storageUrl = System.getenv("FLUTTER_STORAGE_BASE_URL") ?: "https://storage.googleapis.com"
+
     repositories {
         google()
         mavenCentral()
+
+        maven {
+            url = uri("/Users/gi/development/android_webview_example/webViewAndroid/flutter_module/build/host/outputs/repo")
+        }
+        maven {
+            url = uri("$storageUrl/download.flutter.io")
+        }
     }
 }
 
 rootProject.name = "WebView Android"
-include(":app")
+include(":app", ":flutter_module")
