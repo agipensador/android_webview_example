c580e037cab69f00b2ba8bc2cdfbed9aee50ca8b
agipensador <agipensador@gmail.com>
2024-05-27 17:06:12 -0300

    clean

--- WebViewAndroid/app/build.gradle.kts
+++ WebViewAndroid/app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material3.android)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.androidx.navigation.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
